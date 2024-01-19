9d9a19fe4aa3721c264aa7af95a8e2be28b6a91e
sxa69370 <156716103+sxa69370@users.noreply.github.com>
2024-01-19 00:29:47 -0600

    Update program_3.py

--- Assignment-2/program_3.py
+++ Assignment-2/program_3.py
@@ -1,6 +1,6 @@
 # Question: Write a program, which reads heights (inches.) of customers into a list and convert these heights to centimeters in a separate list using: 
-# 1) Nested Interactive loop. 
-# 2) List comprehensions 
+#  1) Nested Interactive loop. 
+#  2) List comprehensions 
 
 l =  [150,155, 145, 148]
  
@@ -11,14 +11,10 @@ def inch_to_cms(l):
     for i in l:
         output1.append(round(i / 2.54,2))
 
-    # List comprehensions
-        
+    # List comprehensions   
     output2 = [round(i/2.54,2) for i in l]
-        
-    
+       
     print("Output from Nested Interactive Loop: {}, Output from List Comprehensions:{}".format(output1,output2))
 
-
-
-
-inch_to_cms(l)
+if __name__ == "__main__":
+  inch_to_cms(l)
