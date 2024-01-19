72cb8cdab44ee3fe6166f4079946df705a93e2c3
sxa69370 <sxa69370@ucmo.edu>
2024-01-19 10:20:21 -0600

    Made the program-3 to take the input from console

--- Assignment-2/program_3.py
+++ Assignment-2/program_3.py
@@ -2,8 +2,10 @@
 #  1) Nested Interactive loop. 
 #  2) List comprehensions 
 
-l =  [150,155, 145, 148]
- 
+s = input("Enter the heights in inches with space in between each and then press enter: ")
+# Converting the string of inches into list by using split function and converting them into float type.
+heights = [float(i) for i in s.split()]
+
 def inch_to_cms(l):
     output1 = []
     output2 = []
@@ -17,4 +19,4 @@ def inch_to_cms(l):
     print("Output from Nested Interactive Loop: {}, Output from List Comprehensions:{}".format(output1,output2))
 
 if __name__ == "__main__":
-  inch_to_cms(l)
+  inch_to_cms(heights)
